477ecc59ba7e01ebe4adb47e9b29fa04ffb28d13
Hano <hinsul3113@gmail.com>
2020-07-17 14:03:47 -0700

    Add user new, create

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :tasks
   get('/complete_task/:id/', to: "tasks#complete_task", as: :complete_task)
 
+  resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 end
