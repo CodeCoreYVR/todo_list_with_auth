cfcd3a38f749b41a966cd6b778fe185645d56cf1
Hano <hinsul3113@gmail.com>
2020-07-17 10:27:16 -0700

    Add task show action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,25 @@
 class TasksController < ApplicationController
     # show the list of tasks
     def index 
-        @tasks = Task.all
+        @tasks = Task.order(created_at: :desc)
     end
+
+    def new
+        @task = Task.new # we need an instance of our resource to be used with 'form_with'
+    end
+
+    def create
+        @task = Task.new(params.require(:task).permit(:title))
+        if @task.save 
+            redirect_to tasks_path
+        else
+            redirect_to new_task_path
+        end
+    end
+
+    def show 
+        id = params[:id]
+        @task = Task.find(id)
+    end
+
 end
