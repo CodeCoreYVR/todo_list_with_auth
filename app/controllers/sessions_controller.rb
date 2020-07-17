5eaa4cebd947511b558b71ced9374f41c20d41cc
Hano <hinsul3113@gmail.com>
2020-07-17 13:53:49 -0700

    Add session destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,4 +27,9 @@ class SessionsController < ApplicationController
             render :new
         end
     end
+
+    def destroy 
+        session[:user_id] = nil 
+        redirect_to tasks_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :tasks
   get('/complete_task/:id/', to: "tasks#complete_task", as: :complete_task)
 
-  resource :session, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 end
