b517d642cb68352f70bad8dafd1ce2f6ed49b4c1
Hano <hinsul3113@gmail.com>
2020-07-17 09:35:00 -0700

    Generate a migration for tasks and seed tasks with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.delete_all
+NUM_TASK = 10
+
+NUM_TASK.times do 
+    created_at = Faker::Date.backward(days: 365 * 5)
+    Task.create(
+        title: Faker::Hacker.say_something_smart,
+        created_at: created_at,
+        updated_at: created_at
+    )
+end
+
+task = Task.all 
+
+puts Cowsay.say("Generate #{task.count} tasks", :bunny)
